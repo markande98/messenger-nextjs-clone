f3fc8f837f7b5dda85645a99128de99c9a990268
markande98 <gauravbpit1998@gmail.com>
2023-09-06 07:12:22 +0530

    react-hook-form setup

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsloading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") setVariant("REGISTER");
+    else setVariant("LOGIN");
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsloading(true);
+
+    if (variant === "REGISTER") {
+      // axios register
+    }
+
+    if (variant === "LOGIN") {
+      // next-auth sign in
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsloading(true);
+
+    //next-auth social sign in
+  };
+
+  return <div>hello</div>;
+};
+
+export default AuthForm;
