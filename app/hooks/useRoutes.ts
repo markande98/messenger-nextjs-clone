d4de13c8f45d2961c75fc07321420cd7f353c94b
markande98 <gauravbpit1998@gmail.com>
2023-09-07 09:24:55 +0530

    desktop sidebar item rendered

---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,42 @@
+import { usePathname } from "next/navigation";
+import useCoversation from "./useConversation";
+import { useMemo } from "react";
+
+import { HiChat, HiUsers } from "react-icons/hi";
+
+import { HiArrowLeftOnRectangle } from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+
+const useRoutes = () => {
+  const pathname = usePathname();
+
+  const { conversationId } = useCoversation();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Chat",
+        href: "/conversations",
+        icon: HiChat,
+        active: pathname === "/conversations" || !!conversationId,
+      },
+      {
+        label: "Users",
+        href: "/users",
+        icon: HiUsers,
+        active: pathname === "/users",
+      },
+      {
+        label: "Logout",
+        href: "#",
+        onClick: () => signOut(),
+        icon: HiArrowLeftOnRectangle,
+      },
+    ],
+    [pathname, conversationId]
+  );
+
+  return routes;
+};
+
+export default useRoutes;
