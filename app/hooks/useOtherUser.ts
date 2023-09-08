2bedffd5ed1af2c8051f37e8898f63b6120cf147
markande98 <gauravbpit1998@gmail.com>
2023-09-08 14:20:32 +0530

    conversation UI started

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -1,26 +1,23 @@
-import { useSession } from "next-auth/react";
-
 import { useMemo } from "react";
-
 import { FullConversationType } from "../types";
-
 import { User } from "@prisma/client";
+import { getSession, useSession } from "next-auth/react";
 
+const useOtherUser = (
+  conversation: FullConversationType | { users: User[] }
+) => {
+  const session = getSession();
+  const otherUser = useMemo(() => {
+    const currentUserEmail = session.then((ses) => ses?.user?.email);
 
-const useOtherUser = (conversation : FullConversationType | {
-    users: User[]
-}) => {
-    const session = useSession();
+    const otherUser = conversation.users.filter(
+      async (user) => user.email !== (await currentUserEmail)
+    );
 
-    const otherUser = useMemo(() => {
-        const currentUserEmail = session?.data?.user?.email;
+    return otherUser[0];
+  }, [session]);
 
-        const otherUser = conversation?.users.filter((user) => user.email !== currentUserEmail);
-
-        return otherUser[0];
-    }, [session?.data?.user?.email, conversation])
-
-    return otherUser;
-}
+  return otherUser;
+};
 
 export default useOtherUser;
